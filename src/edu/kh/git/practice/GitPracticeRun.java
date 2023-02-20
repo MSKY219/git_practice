65d3c53002e038448a573e41b4c875ae43e1382d
MS.K <cropruch@naver.com>
2023-02-20 19:20:05 +0900

    두 번째 커밋.

--- src/edu/kh/git/practice/GitPracticeRun.java
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -4,9 +4,11 @@ public class GitPracticeRun {
 
 	public static void main(String[] args) {
 		System.out.println("안녕하세요1.");
-		System.out.println("안녕하세요2.");
 		System.out.println("안녕하세요3.");
 		System.out.println("GIT_PRACTICE_FILE");
+		System.out.println("추가 1");
+		System.out.println("추가 2");
+		System.out.println("추가 3");
 	}
 
 }
