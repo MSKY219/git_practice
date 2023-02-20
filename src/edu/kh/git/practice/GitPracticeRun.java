92552fd7c31acbc28cc7150489319c6cca21249d
MS.K <cropruch@naver.com>
2023-02-20 19:16:15 +0900

    첫 번째 커밋 생성.

---
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -0,0 +1,12 @@
+package edu.kh.git.practice;
+
+public class GitPracticeRun {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요1.");
+		System.out.println("안녕하세요2.");
+		System.out.println("안녕하세요3.");
+		System.out.println("GIT_PRACTICE_FILE");
+	}
+
+}
